a8035c7d227962bdde0d16799008747874168cf4
Chris Roman <Christopherroman85@gmail.com>
2020-07-27 13:45:37 -0500

    Logic is working to create reservations, beginning templates for views.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,7 +4,6 @@ class ReservationsController < ApplicationController
 
   def index
     @reservations = Reservation.order('res_day DESC')
-    res_list_by("Sat", "7PM")
   end
   
   def new
@@ -13,6 +12,7 @@ class ReservationsController < ApplicationController
 
   def create
     table_id = confirm_reservation_with(params[:res_day], params[:res_time], params[:party_size])
+    puts table_id
     if table_id
       @reservation = Reservation.new(res_name:params[:res_name], res_time:params[:res_time], party_size:params[:party_size], res_day:params[:res_day], tabletops_id:table_id)
       if @reservation.save
@@ -20,6 +20,8 @@ class ReservationsController < ApplicationController
       else
         puts 'Something went wrong.'
       end
+    elsif !table_id
+      puts "There are no available tables for the selected time."
     else
       puts "An error occurred after attempting to confirm reservation method"
     end
@@ -32,35 +34,89 @@ class ReservationsController < ApplicationController
   end
 
   def res_list_by day, time
-    reservations = Reservation.where(res_day: day, res_time: time)
     occupied_tables = []
-    reservations.each do |res|
-      occupied_tables << res.tabletops_id
+    reservations = Reservation.where(res_day: day, res_time: time)
+    if reservations.length > 0
+      reservations.each do |res|
+        occupied_tables << res.tabletops_id
+      end
+      return occupied_tables
+    else
+      return nil
     end
-    return occupied_tables 
   end
 
   def tabletops_that_meet_requested seats
+    table_numbers = []
     tabletops = Tabletop.where("seats >= ?", seats)
-    return tabletops
+    if tabletops.length > 0
+      tabletops.each do |table|
+        table_numbers << table.id
+      end
+      puts table_numbers
+      return table_numbers
+    else
+      return nil
+    end
   end
 
   def confirm_reservation_with day, time, seats
     current_reservations = res_list_by(day, time)
+
     compatable_tables = tabletops_that_meet_requested(seats)
-    if (current_reservations.length >= 10)
-      puts "There are no available tables for the selected time and party size."
-    else
-      table_options = current_reservations.reject do |i|
-        compatable_tables.include?(i)
+
+    res_table_id = nil
+
+    if !current_reservations
+      res_table_id = compatable_tables[0]
+      return res_table_id
+    elsif current_reservations.length >= 10
+      return res_table_id
+    else 
+
+      table_options = compatable_tables.reject do |i|
+        current_reservations.include?(i)
+      end
+      puts "====reject/inlude===="
+      puts table_options
+      puts "====reject/inlude===="
+      # table_options = current_reservations + compatable_tables
+      # table_options.uniq
+      # puts "====after uniq===="
+      # puts table_options
+      # puts "====after uniq===="
+
+      if table_options === 0
+        return res_table_id
+      else
+        res_table_id = table_options[0]
+        return res_table_id
       end
     end
-    if table_options
-      res_table_id = table_options[0]
-    else
-      puts "There are no available tables for the selected time and party size."
-    end
-    return res_table_id
   end
+
+  # def confirm_reservation_with day, time, seats
+  #   #Get all reservations tabletop IDs for specific date and time
+  #   current_reservations = res_list_by(day, time)
+  #   #Get all compatable tables based on the party size
+  #   compatable_tables = tabletops_that_meet_requested(seats)
+  #   #If res_list_by returns nil do
+  #   table_options = nil
+  #   if current_reservations === nil
+  #     res_table_id = compatable_tables[0]
+  #     return res_table_id
+  #   #If there are 10 reservations for the date and time return error
+  #   elsif current_reservations.length >= 10
+  #     return "There are no available tables for the selected time and party size."
+  #   else
+  #     table_options = (current_reservations + compatable_tables - (current_reservations & compatable_tables))
+  #   end
+  #   if table_options != nil
+  #     res_table_id = table_options[0]
+  #   else
+  #     return "There are no available tables for the selected time and party size."
+  #   end
+  #   return res_table_id
+  # end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tabletops = Tabletop.create([
+  {
+	"seats":"2",
+	"table_name":"TableOne"
+  },
+  {
+	"seats":"2",
+	"table_name":"TableTwo"
+  },
+  {
+	"seats":"2",
+	"table_name":"TableThree"
+  },
+  {
+	"seats":"4",
+	"table_name":"TableFour"
+  },
+  {
+	"seats":"4",
+	"table_name":"TableFive"
+  },
+  {
+	"seats":"4",
+	"table_name":"TableSix"
+  },
+  {
+	"seats":"6",
+	"table_name":"TableSeven"
+  },
+  {
+	"seats":"6",
+	"table_name":"TableEight"
+  },
+  {
+	"seats":"8",
+	"table_name":"TableNine"
+  },
+  {
+	"seats":"8",
+	"table_name":"TableTen"
+  },
+])
