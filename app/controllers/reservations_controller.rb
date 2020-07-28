f1c06fc1535397bdd1e40372d9b824f0b24d5f92
Chris Roman <Christopherroman85@gmail.com>
2020-07-28 14:08:18 -0500

    logic is working, front end styling nearly done.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,24 +6,23 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.order('res_day DESC')
   end
   
-  def new
+  def _new
     @reservation = Reservation.new
   end
 
   def create
     table_id = confirm_reservation_with(params[:res_day], params[:res_time], params[:party_size])
-    puts table_id
     if table_id
       @reservation = Reservation.new(res_name:params[:res_name], res_time:params[:res_time], party_size:params[:party_size], res_day:params[:res_day], tabletops_id:table_id)
       if @reservation.save
-        puts 'Reservation Created'
+        redirect_to '/reservations', notice: "Reservation confirmed!"
       else
-        puts 'Something went wrong.'
+        redirect_to '/reservations', error: "Error occured during booking process, please try again."
       end
     elsif !table_id
-      puts "There are no available tables for the selected time."
+      redirect_to '/reservations', warning: "There are no tables available for the selected date and time."
     else
-      puts "An error occurred after attempting to confirm reservation method"
+      redirect_to '/reservations', error: "An error occurred after attempting to confirm reservation method"
     end
   end
 
@@ -53,7 +52,6 @@ class ReservationsController < ApplicationController
       tabletops.each do |table|
         table_numbers << table.id
       end
-      puts table_numbers
       return table_numbers
     else
       return nil
@@ -61,62 +59,51 @@ class ReservationsController < ApplicationController
   end
 
   def confirm_reservation_with day, time, seats
+    #Get all reservations tabletop IDs for specific date and time
     current_reservations = res_list_by(day, time)
 
-    compatable_tables = tabletops_that_meet_requested(seats)
-
-    res_table_id = nil
-
     if !current_reservations
-      res_table_id = compatable_tables[0]
-      return res_table_id
+      compatable_tables = tabletops_that_meet_requested(seats)
+      if !compatable_tables
+        return nil
+      else
+        res_table_id = compatable_tables[0]
+        return res_table_id
+      end
     elsif current_reservations.length >= 10
-      return res_table_id
-    else 
-
+      return nil
+    else
+     #Get all compatable tables based on the party size
+      compatable_tables = tabletops_that_meet_requested(seats)
       table_options = compatable_tables.reject do |i|
         current_reservations.include?(i)
       end
-      puts "====reject/inlude===="
-      puts table_options
-      puts "====reject/inlude===="
-      # table_options = current_reservations + compatable_tables
-      # table_options.uniq
-      # puts "====after uniq===="
-      # puts table_options
-      # puts "====after uniq===="
-
-      if table_options === 0
-        return res_table_id
-      else
-        res_table_id = table_options[0]
-        return res_table_id
-      end
+      res_table_id = table_options[0]
+      return res_table_id
     end
   end
 
-  # def confirm_reservation_with day, time, seats
-  #   #Get all reservations tabletop IDs for specific date and time
-  #   current_reservations = res_list_by(day, time)
-  #   #Get all compatable tables based on the party size
-  #   compatable_tables = tabletops_that_meet_requested(seats)
-  #   #If res_list_by returns nil do
-  #   table_options = nil
-  #   if current_reservations === nil
+
+
+
+  #   # No reservations booked during time/day
+  #   if !current_reservations
   #     res_table_id = compatable_tables[0]
   #     return res_table_id
-  #   #If there are 10 reservations for the date and time return error
+  #   # All tables are occupied during time/day
   #   elsif current_reservations.length >= 10
-  #     return "There are no available tables for the selected time and party size."
+  #     return res_table_id
+  #   # Remove occupied tables from table array
   #   else
-  #     table_options = (current_reservations + compatable_tables - (current_reservations & compatable_tables))
+  #     # No compatable tables after dupes removed
+  #     if table_options === 0
+  #       return res_table_id
+  #     # Select first available table in table array
+  #     else
+  #       res_table_id = table_options[0]
+  #       return res_table_id
+  #     end
   #   end
-  #   if table_options != nil
-  #     res_table_id = table_options[0]
-  #   else
-  #     return "There are no available tables for the selected time and party size."
-  #   end
-  #   return res_table_id
   # end
   
 end
